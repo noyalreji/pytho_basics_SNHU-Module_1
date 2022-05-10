d6c1f190b9792142349c3aa0aa9ea5eaae9ddf80
Noyal Reji Jacob <84338635+noyalreji@users.noreply.github.com>
2022-05-10 02:27:28 -0500

    Create m1.py

---
+++ m1.py
@@ -0,0 +1,10 @@
+#Complete the program to read four values from input and store the values in variables first_name, generic_location, whole_number, and plural_noun. The program uses the input values to output a short story.
+first_name = input()
+generic_location = input()
+whole_number = input()
+plural_noun = input()
+
+
+
+# Output a short story using the four input values. Do not modify the code below.
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
