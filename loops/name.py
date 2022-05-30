266d727692553e239806588c848e7e77a2848e4e
Noyal Reji Jacob <84338635+noyalreji@users.noreply.github.com>
2022-05-29 19:34:26 -0500

    Create name.py

---
+++ loops/name.py
@@ -0,0 +1,12 @@
+contact_emails = {
+    'Sue Reyn' : 's.reyn@email.com',
+    'Mike Filt': 'mike.filt@bmail.com',
+    'Nate Arty': 'narty042@nmail.com'
+}
+
+new_contact = input()
+new_email = input()
+contact_emails[new_contact] = new_email
+
+for k, v in contact_emails.items():  # go through all contact information
+    print(v, "is", k)  # print name and mail
